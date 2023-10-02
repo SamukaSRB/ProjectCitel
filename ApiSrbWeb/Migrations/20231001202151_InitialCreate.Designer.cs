cf2e8ca718589a6e3944dbe7752cb427c1c378e6
Samuel <samuka-srb@live.com>
2023-10-01 21:42:57 -0300

    test: ProjectCitel => "Adiciona migratios de modelo"

--- ApiSrbWeb/Migrations/20231001202151_InitialCreate.Designer.cs
+++ ApiSrbWeb/Migrations/20231001202151_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ApiSrbWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiSrbWeb.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231001202151_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
