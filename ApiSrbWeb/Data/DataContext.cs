0dc7d526b599544640bf4a391b083f64a7a5e586
Samuel <samuka-srb@live.com>
2023-10-01 21:33:13 -0300

    fix: ProjectCitel => "Correção de referencia cruzada Newtonsoft"

--- ApiSrbWeb/Data/DataContext.cs
+++ ApiSrbWeb/Data/DataContext.cs
@@ -6,23 +6,25 @@ namespace ApiSrbWeb.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }      
-        public DbSet<Category> Categories { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+      
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Category>().HasKey(c => c.CategoryId);
-            modelBuilder.Entity<Category>().Property(c => c.CategoryName).HasMaxLength(100).IsRequired();
-            modelBuilder.Entity<Category>().Property(c => c.CategoryDescription).HasMaxLength(150).IsRequired();
+            //modelBuilder.Entity<Category>().HasKey(c => c.CategoryId);
+            //modelBuilder.Entity<Category>().Property(c => c.CategoryName).HasMaxLength(100).IsRequired();
+            //modelBuilder.Entity<Category>().Property(c => c.CategoryDescription).HasMaxLength(150).IsRequired();
 
-            modelBuilder.Entity<Product>().HasKey(c => c.ProductId);
-            modelBuilder.Entity<Product>().Property(c => c.ProductEan).HasMaxLength(100).IsRequired();
-            modelBuilder.Entity<Product>().Property(c => c.ProductName).HasMaxLength(100).IsRequired();
-            modelBuilder.Entity<Product>().Property(c => c.ProductDescription).HasMaxLength(150).IsRequired();
-            modelBuilder.Entity<Product>().Property(c => c.ProductPrice).HasPrecision(14,2).IsRequired();
-            modelBuilder.Entity<Product>().Property(c => c.ProductStock).HasMaxLength(100).IsRequired();
-            modelBuilder.Entity<Product>().Property(c => c.ProductImageUrl).HasMaxLength(100).IsRequired();
+            //modelBuilder.Entity<Product>().HasKey(c => c.ProductId);
+            //modelBuilder.Entity<Product>().Property(c => c.ProductEan).HasMaxLength(100).IsRequired();
+            //modelBuilder.Entity<Product>().Property(c => c.ProductName).HasMaxLength(100).IsRequired();
+            //modelBuilder.Entity<Product>().Property(c => c.ProductDescription).HasMaxLength(150).IsRequired();
+            //modelBuilder.Entity<Product>().Property(c => c.ProductPrice).HasPrecision(14,2).IsRequired();
+            //modelBuilder.Entity<Product>().Property(c => c.ProductStock).HasMaxLength(100).IsRequired();
+            //modelBuilder.Entity<Product>().Property(c => c.ProductImageUrl).HasMaxLength(100).IsRequired();
 
             modelBuilder.Entity<Product>()
                 .HasOne<Category>(c => c.Category)
